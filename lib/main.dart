b1d028613e2775558fc0464548f9fb44534587bd
leidiane <leidiane.igtu@gmail.com>
2024-03-08 16:52:18 -0300

    style: Mudar a cor da barra superior

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          inversePrimary: Colors.amber),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -102,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          // wireframe for each widget.          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
