cd3cd739f530859cf3e8b192271b4d0d8f474d95
kamilateofilo <123755464+kamilateofilo@users.noreply.github.com>
2024-03-11 15:29:49 -0300

    kamila/style/add-new-icon
    style button: troca de ícone do botão

--- lib/main.dart
+++ lib/main.dart
@@ -118,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.favorite),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
